83e78ee9cf6e843f64e6ba0a8c3af715ec368e47
Fabio Rueda <avances123@gmail.com>
2018-05-07 00:14:06 +0200

    funciona un poco

--- app/app-module.es6
+++ app/app-module.es6
@@ -17,7 +17,6 @@
       'home',
       'toolbar',
       'login',
-      'me',
       'farm',
       'leftnav'
     ])
--- app/js/environments.js
+++ app/js/environments.js
@@ -1,2 +1,2 @@
-angular.module('env-config', [])
+angular.module("env-config", [])
 .constant("EnvironmentConfig", {"api":"http://localhost:8000/"});
