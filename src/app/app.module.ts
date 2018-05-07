9ab841e57773a4f31d1cba84cbff0cc1b0fe9ec4
Fabio Rueda <avances123@gmail.com>
2018-05-08 00:19:51 +0200

    meto gridster2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
+import { GridsterModule } from 'angular-gridster2';
 
 
 import { AppComponent } from './app.component';
 import { FarmListComponent } from './farm-list/farm-list.component';
+import { GridsterCompact } from 'angular-gridster2/lib/gridsterCompact.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { FarmListComponent } from './farm-list/farm-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    GridsterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
