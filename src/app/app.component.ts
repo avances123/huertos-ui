8cd7db10f54204125cd817a5978d24fcd057301b
Fabio Rueda <avances123@gmail.com>
2018-05-21 21:33:53 +0200

    me deslogueo y me voy a login, y quito la sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { AuthService} from './auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,8 +12,11 @@ import { AuthService} from './auth.service';
 })
 export class AppComponent {
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-  constructor(private breakpointObserver: BreakpointObserver, public authService: AuthService) {}
+  constructor(private breakpointObserver: BreakpointObserver, public authService: AuthService,private router: Router) {}
+
   logout() {
     this.authService.logout();
+    this.router.navigate(['/login']);    
   }
+  
 }
